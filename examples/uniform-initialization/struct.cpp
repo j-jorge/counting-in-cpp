149272ab7f3dc08a1b63d59a6246f79eb1f0781e
Julien Jorge <julien.jorge@stuff-o-matic.com>
2021-04-25 18:33:31 +0200

    Uniform initialization.

---
+++ examples/uniform-initialization/struct.cpp
@@ -0,0 +1,52 @@
+#include <cstdio>
+#include <utility>
+
+struct foo_struct
+{
+  int a;
+  int b;
+};
+
+struct foo_constructor
+{
+  foo_constructor(int v1, int v2)
+    // Note that arguments and fields are swapped.
+    : a(v2), b(v1)
+  {}
+
+  int a;
+  int b;
+};
+
+struct foo_initializer_list
+{
+  foo_initializer_list(int v1, int v2)
+    // Note that arguments and fields are still swapped.
+    : a(v2), b(v1)
+  {}
+
+  // We will come back to std::initializer list later :)
+  foo_initializer_list(const std::initializer_list<int>& i)
+    // Note that the fields are set to the same value.
+    : a(*i.begin()),
+      b(a)
+  {}
+
+  int a;
+  int b;
+};
+
+template<typename Foo>
+void build_foo(int x, int y)
+{
+  Foo foo{x, y};
+
+  printf(".a=%d, .b=%d\n", foo.a, foo.b);
+}
+
+int main()
+{
+  build_foo<foo_struct>(24, 42);
+  build_foo<foo_constructor>(24, 42);
+  build_foo<foo_initializer_list>(24, 42);
+}
